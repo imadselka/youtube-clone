4f65dde5bbe37a849d90a32aa8d7b05c99ba4ff9
Imad Selka <imadselkawi133@gmail.com>
2024-01-22 12:29:50 +0100

    fix : page wasnt loading

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,7 +6,7 @@ import { Videos, Sidebar } from "./";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
-  const [videos, setVideos] = useState(null);
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     setVideos(null);
